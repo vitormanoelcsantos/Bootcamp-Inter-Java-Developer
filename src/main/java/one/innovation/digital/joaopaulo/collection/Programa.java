798ec75ee4176f1b9720936ad293f0c08cb91c1e
vitormanoelcsantos <vitormanoelcsantos@outlook.com>
2021-03-09 18:48:10 -0300

    Add files viad upload

--- src/main/java/one/innovation/digital/joaopaulo/collection/Programa.java
+++ src/main/java/one/innovation/digital/joaopaulo/collection/Programa.java
@@ -1,7 +1,8 @@
 package one.innovation.digital.joaopaulo.collection;
 
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
+
 public class Programa {
     public static void main(String[] args) {
         Collection<String> nomes = List.of("Vitor", "Manoel", "Carneiro", "dos", "Santos");
